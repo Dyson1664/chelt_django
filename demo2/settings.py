7e0862581fb4d24f1feae8d502dee38d37941258
david reilly <davidreilly02@gmail.com>
2024-02-23 09:15:21 +0700

    added more logging

--- demo2/settings.py
+++ demo2/settings.py
@@ -178,17 +178,36 @@ CACHES = {
         },
     }
 }
-
+import sys
 LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
         },
     },
-    "root": {
-        "handlers": ["console"],
-        "level": "WARNING",
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'stream': sys.stdout,
+            'formatter': 'verbose'
+        },
     },
+    'loggers': {
+        'django': {
+            'handlers':['console'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'myapp2': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
 }
