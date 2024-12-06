4764ac41f4f957b63afa628541eba1fd86448294
david reilly <davidreilly02@gmail.com>
2024-12-06 16:16:14 +0000

    removed border for video

--- demo2/settings.py
+++ demo2/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # DEBUG = 'RENDER' not in os.environ
 
 
